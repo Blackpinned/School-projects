a21e15a8bb41b08a1862aede813465053c0b3264
Mathieu Dewolf <mathie.dewolf@gmail.com>
2018-12-26 14:08:23 +0100

    JDBC Repos updated

--- Retrovideo/src/main/java/be/vdab/Retrovideo/repositories/JDBCGenreRepository.java
+++ Retrovideo/src/main/java/be/vdab/Retrovideo/repositories/JDBCGenreRepository.java
@@ -1,24 +1,44 @@
 package be.vdab.Retrovideo.repositories;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
 import be.vdab.Retrovideo.enitities.Genre;
 
+@Repository
 public class JDBCGenreRepository implements GenreRepository {
+	
+	private final RowMapper<Genre> genreRowMapper =
+			(resultSet, rowNum) -> new Genre(resultSet.getLong("id"), resultSet.getString("naam"));
+	private final NamedParameterJdbcTemplate template;
+	private final String SELECT_ALL = "select id, naam from genres order by id";
+	private final String READ = "select id, naam from genres where id = :id";
+
+	public JDBCGenreRepository(NamedParameterJdbcTemplate template) {
+
+		this.template = template;
+	}
 	
 	@Override
 	public Optional<Genre> read(long id) {
 
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return Optional.of(template.queryForObject(READ, Collections.singletonMap("id", id), genreRowMapper));
+		} catch (final IncorrectResultSizeDataAccessException ex) {
+			return Optional.empty();
+		}
 	}
 	
 	@Override
 	public List<Genre> findAll() {
 
-		// TODO Auto-generated method stub
-		return null;
+		return template.query(SELECT_ALL, genreRowMapper);
 	}
 	
 }
--- Retrovideo/src/main/java/be/vdab/Retrovideo/repositories/JDBCKlantRepository.java
+++ Retrovideo/src/main/java/be/vdab/Retrovideo/repositories/JDBCKlantRepository.java
@@ -1,24 +1,44 @@
 package be.vdab.Retrovideo.repositories;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
 import be.vdab.Retrovideo.enitities.Klant;
 
+@Repository
 public class JDBCKlantRepository implements KlantRepository {
+
+	private final RowMapper<Klant> klantRowMapper =
+			(resultSet, rowNum) -> new Klant(resultSet.getLong("id"), resultSet.getString("familienaam"), resultSet.getString("voornaam"), resultSet.getString("straatNummer"), resultSet.getString("postcode"), resultSet.getString("gemeente"));
+	private final NamedParameterJdbcTemplate template;
+	private final String SELECT_ALL = "select id, naam from genres order by id";
+	private final String READ = "select id, naam from genres where id = :id";
+
+	public JDBCKlantRepository(NamedParameterJdbcTemplate template) {
+
+		this.template = template;
+	}
 	
 	@Override
 	public Optional<Klant> read(long id) {
-
-		// TODO Auto-generated method stub
-		return null;
+		
+		try {
+			return Optional.of(template.queryForObject(READ, Collections.singletonMap("id", id), klantRowMapper));
+		} catch (final IncorrectResultSizeDataAccessException ex) {
+			return Optional.empty();
+		}
 	}
-	
+
 	@Override
 	public List<Klant> findAll() {
-
-		// TODO Auto-generated method stub
-		return null;
+		
+		return template.query(SELECT_ALL, klantRowMapper);
 	}
-
+	
 }
