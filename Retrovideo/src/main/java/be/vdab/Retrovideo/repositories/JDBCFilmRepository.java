1a3ef5854569a3c7040821b41b5cf234cb32f6ab
Mathieu Dewolf <mathie.dewolf@gmail.com>
2018-12-26 14:57:58 +0100

    JDBC updates en Filmrep test

--- Retrovideo/src/main/java/be/vdab/Retrovideo/repositories/JDBCFilmRepository.java
+++ Retrovideo/src/main/java/be/vdab/Retrovideo/repositories/JDBCFilmRepository.java
@@ -16,7 +16,7 @@ import be.vdab.Retrovideo.exceptions.FilmNietGevondenException;
 
 @Repository
 public class JDBCFilmRepository implements FilmRepository {
-
+	
 	private final NamedParameterJdbcTemplate template;
 	private static final String UPDATE_FILM =
 			"update films set voorraad = :voorraad, gereserveerd = :gereserveerd where id = :id";
@@ -24,18 +24,18 @@ public class JDBCFilmRepository implements FilmRepository {
 			resultSet.getLong("id"), resultSet.getLong("genreid"), resultSet.getString("titel"), resultSet.getInt("voorraad"), resultSet.getInt("gereserveerd"), resultSet.getBigDecimal("prijs")
 	);
 	private static final String SELECT_ALL =
-			"select id, genreid, titel, voorraad, gereserveerd, prijs from fims order by id";
+			"select id, genreid, titel, voorraad, gereserveerd, prijs from films order by id";
 	private static final String READ =
-			"select id, genreid, titel, voorraad, gereserveerd, prijs from fims where id = :id";
-
+			"select id, genreid, titel, voorraad, gereserveerd, prijs from films where id = :id";
+	
 	public JDBCFilmRepository(NamedParameterJdbcTemplate template) {
-		
+
 		this.template = template;
 	}
-
+	
 	@Override
 	public void update(Film film) {
-		
+
 		final Map<String, Object> parameters = new HashMap<>();
 		parameters.put("voorraad", film.getVoorraad());
 		parameters.put("gereserveerd", film.getGereserveerd());
@@ -44,21 +44,21 @@ public class JDBCFilmRepository implements FilmRepository {
 			throw new FilmNietGevondenException();
 		}
 	}
-
+	
 	@Override
 	public Optional<Film> read(long id) {
-		
+
 		try {
 			return Optional.of(template.queryForObject(READ, Collections.singletonMap("id", id), filmRowMapper));
 		} catch (final IncorrectResultSizeDataAccessException ex) {
 			return Optional.empty();
 		}
 	}
-
+	
 	@Override
 	public List<Film> findAll() {
-		
+
 		return template.query(SELECT_ALL, filmRowMapper);
 	}
-	
+
 }
---
+++ Retrovideo/src/test/java/be/vdab/Retrovideo/repositories/JDBCFilmRepositoryTest.java
@@ -0,0 +1,77 @@
+package be.vdab.Retrovideo.repositories;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import be.vdab.Retrovideo.enitities.Film;
+import be.vdab.Retrovideo.exceptions.FilmNietGevondenException;
+
+@RunWith(SpringRunner.class)
+@JdbcTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Import(JDBCFilmRepository.class)
+@Sql("/insertFilm.sql")
+public class JDBCFilmRepositoryTest extends AbstractTransactionalJUnit4SpringContextTests {
+
+	private static final String FILMS = "films";
+	@Autowired
+	private JDBCFilmRepository repository;
+	
+	@Test
+	public void findAll() {
+		
+		final List<Film> films = repository.findAll();
+		assertEquals(super.countRowsInTable(FILMS), films.size());
+
+		long vorigeId = 0;
+		for (final Film film : films) {
+			assertTrue(film.getId() > vorigeId);
+			vorigeId = film.getId();
+		}
+	}
+
+	private long idVanTestFilm() {
+
+		return super.jdbcTemplate.queryForObject("select id from films where titel = 'test'", Long.class);
+	}
+
+	public void read() {
+		
+		assertEquals("test", repository.read(idVanTestFilm()).get().getTitel());
+	}
+
+	public void readOnbestaandePizza() {
+		
+		assertFalse(repository.read(-1).isPresent());
+	}
+
+	@Test
+	public void update() {
+		
+		final long id = idVanTestFilm();
+		final Film film = new Film(id, 2, "test", 5, 0, BigDecimal.ONE);
+		repository.update(film);
+		assertEquals(5, repository.read(idVanTestFilm()).get().getVoorraad());
+	}
+
+	@Test(expected = FilmNietGevondenException.class)
+	public void updateOnbestaandePizza() {
+		
+		repository.update(new Film(-1, 2, "test", 5, 0, BigDecimal.ONE));
+	}
+}
