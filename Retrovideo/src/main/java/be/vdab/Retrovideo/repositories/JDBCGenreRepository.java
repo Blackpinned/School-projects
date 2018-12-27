6277f85d4c7cb70cd072f4e78987786808375550
Mathieu Dewolf <mathie.dewolf@gmail.com>
2018-12-27 23:23:18 +0100

    Repository test classes gemaakt en kleine aanpassingen aan classes

--- Retrovideo/src/main/java/be/vdab/Retrovideo/repositories/JDBCGenreRepository.java
+++ Retrovideo/src/main/java/be/vdab/Retrovideo/repositories/JDBCGenreRepository.java
@@ -13,32 +13,32 @@ import be.vdab.Retrovideo.enitities.Genre;
 
 @Repository
 public class JDBCGenreRepository implements GenreRepository {
-	
+
 	private final RowMapper<Genre> genreRowMapper =
 			(resultSet, rowNum) -> new Genre(resultSet.getLong("id"), resultSet.getString("naam"));
 	private final NamedParameterJdbcTemplate template;
-	private final String SELECT_ALL = "select id, naam from genres order by id";
+	private final String SELECT_ALL = "select id, naam from genres order by naam";
 	private final String READ = "select id, naam from genres where id = :id";
-
+	
 	public JDBCGenreRepository(NamedParameterJdbcTemplate template) {
-
+		
 		this.template = template;
 	}
-	
+
 	@Override
 	public Optional<Genre> read(long id) {
-
+		
 		try {
 			return Optional.of(template.queryForObject(READ, Collections.singletonMap("id", id), genreRowMapper));
 		} catch (final IncorrectResultSizeDataAccessException ex) {
 			return Optional.empty();
 		}
 	}
-	
+
 	@Override
 	public List<Genre> findAll() {
-
+		
 		return template.query(SELECT_ALL, genreRowMapper);
 	}
-	
+
 }
